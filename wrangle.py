7c422014b2edf60bb23d4fe46f08aa29e2411200
JaredVahle <jaredjohnvahle@gmail.com>
2021-10-19 17:18:52 -0500

    updating work

--- wrangle.py
+++ wrangle.py
@@ -15,28 +15,5 @@ def wrangle_cohort_data():
     info_df = acquire.get_cohort_information_data()
     # Joining the dataframes
     df = df.join(info_df,on = 'cohort_id',how = 'outer',lsuffix = 'str')
-    # Deleting the columns that had over 700000+ nulls
-    df.drop(columns = ['id',
-    'namestr',
-    'slack',
-    'start_datestr',
-    'end_datestr',
-    'created_at',
-    'updated_at',
-    'deleted_at',
-    'program_idstr'],inplace = True)
-    # Renaming the column for readability
-    df.rename(columns = {'name':'cohort_name'})
-    # Dropping the 6 null values for path
-    df.dropna(inplace = True)
-    # Changing the cohort id into an int for astetics
-    df = df.astype({"cohort_id": int})
-    # Moving the datetime column to my index
-    df['datetime'] = df.date + ' ' + df.time
-    df['datetime'] = pd.to_datetime(df['datetime'])
-    df = df.set_index('datetime')
-    # Making new columns to help me.
-    df['count_helper'] = 1
-    df['program_name'] = df.program_id.replace({1:'PHP Full Stack Web Development',2:'Java Full Stack Web Development',3:'Data Science',4:'Front End Web Development'})
-    df['program_subdomain'] = df.program_id.replace({1:'php',2:'java',3:'ds',4:'fe'})
+    df = prepare.clean_cohort_data(df)
     return df
