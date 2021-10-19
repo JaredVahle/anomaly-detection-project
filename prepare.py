9058e89f3885527e0c34c71640479092c02610a4
JaredVahle <jaredjohnvahle@gmail.com>
2021-10-19 13:16:59 -0500

    updating work

--- prepare.py
+++ prepare.py
@@ -10,4 +10,8 @@ def clean_cohort_data(df):
     df['program_subdomain'] = df.program_id.replace({1:'php',2:'java',3:'ds',4:'fe'})
     df.drop(columns = ['date','time','deleted_at','program_id','id'],inplace = True)
     df.rename(columns = {'name':'cohort_name'},inplace = True)
+    dictionary = {'Bash':61,'Hyperion':58,'Darden':59,'Florence':137,'Jupiter':62}
+    df['cohort_id'].fillna(df['cohort_name'].map(dictionary), inplace=True)
+    df = df.astype({"cohort_id": int})
+    df['count_helper'] = 1
     return df
